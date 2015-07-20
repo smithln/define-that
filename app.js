f242fc0a5ede3e13bd8d7a5cb27664beea926356
Lauren Smith <laurenicolesmith@gmail.com>
2015-07-20 08:01:18 -0500

    Adds marked package for markdown usage

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
 
+// Use Markdown via Marked
+var marked = require('marked');
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+console.log(marked('I am using __markdown__.'));
+
+// Homepage
 app.get('/', function (req, res) {
-  res.send('This is the place');
+  res.send('I am the homepage');
 });
 
 var server = app.listen(3000, function () {
