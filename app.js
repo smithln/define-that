9fccbfc398f134b7cc0eb8fe07194bcac8065eff
Lauren Smith <laurenicolesmith@gmail.com>
2015-07-20 08:18:34 -0500

    Adds definition routes, npm start script

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var path = require('path');
+var fs = require('fs');
+
+// pageMissing function
+function pageMissing(respond) {
+  respond.status(404) + '.' + respond.send('Not Found');
+ };
+
 // Use Markdown via Marked
 var marked = require('marked');
 marked.setOptions({
@@ -18,6 +26,25 @@ console.log(marked('I am using __markdown__.'));
 // Homepage
 app.get('/', function (req, res) {
   res.send('I am the homepage');
+});
+
+// Definititon Pages
+app.get('/definitions/:def_id', function(req, res) {
+  var def_id = req.params.def_id;
+  var path = __dirname + '/definitions/' + def_id + '.md';
+  var file = fs.readFile(path, function(err, data) {
+    if (err) {
+      pageMissing(res)
+    }
+    if (data) {
+      return res.send(marked(data.toString()));
+    }
+  });
+});
+
+// 404
+app.use(function(req, res, next) {
+  pageMissing(res);
 });
 
 var server = app.listen(3000, function () {
---
+++ definitions/sass.md
@@ -0,0 +1,10 @@
+# Sass
+
+**Developers**
+- Hampton Catlin
+
+Sass lets you use features that don't exist in CSS yet like variables, nesting, mixins,
+inheritance and other nifty goodies that make writing CSS fun again.
+
+**To Install**
+[Sass Website Instructions](http://sass-lang.com/install)
